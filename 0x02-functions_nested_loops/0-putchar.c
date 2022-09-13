9961d512de728e430d92d6747f8364d2a57c00ec
Akintayoismail <ismailakintayo@icloud.com>
2022-09-13 15:37:24 +0000

    Task 0. _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * The prototypes of all your functions and the prototype of the function _putchar
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+}
