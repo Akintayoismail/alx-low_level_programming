594ba61cf3728f50c4b86a2ee292906535010ca1
Akintayoismail <ismailakintayo@icloud.com>
2022-10-12 14:27:35 +0000

    Task 0. What's my name

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,11 +1,5 @@
-#ifndef FUNCTION_POINTERS_H
-#define FUNCTION_POINTERS_H
-
-/* Description: Header file containing prototypes for all functions
- *       used in the 0x0E-function_pointers directory.
-*/
-
-#include <stdlib.h>
+#ifndef FUCTION_POINTERS_H
+#define FUCTION_POINTERS_H
 
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
